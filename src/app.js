5c36f828371751a42c93f638250f2ca03f751e56
Stephen Downing <stephen.downing@amtrustgroup.com>
2019-05-31 15:23:10 -0400

    Picking An Option

--- src/app.js
+++ src/app.js
@@ -20,9 +20,15 @@ const onFormSubmit = (e) => {
     }
 };
 
-const wipeArray = () => {
+const onRemoveAll = () => {
     app.options = [];
     renderIndecisionApp();
+};
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
 };
 
 
@@ -36,8 +42,8 @@ const renderIndecisionApp = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <p>{app.options.length}</p>
-            <button onClick={wipeArray}>Remove All</button>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+            <button onClick={onRemoveAll}>Remove All</button>
             <ol>
                 {
                     app.options.map((option) => {
